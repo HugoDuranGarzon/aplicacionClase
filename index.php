cb78ded5e7557bd51ede429cde790cd5955f50dc
Hugo Duran Garzon <hugodurangarzon@gmail.com>
2024-10-29 11:01:25 +0100

    primer commit

---
+++ index.php
@@ -0,0 +1,88 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <title>Title</title>
+        <!-- Required meta tags -->
+        <meta charset="utf-8" />
+        <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1, shrink-to-fit=no"
+        />
+
+        <!-- Bootstrap CSS v5.2.1 -->
+        <link
+            href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
+            rel="stylesheet"
+            integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN"
+            crossorigin="anonymous"
+        />
+    </head>
+
+    <body style="background-image:url(https://conviveyestudia.org/wp-content/uploads/2020/04/aula-c-santo-domingo-coruna.jpg); background-repeat: no-repeat; background-size:cover; background-position: center;">
+        <header>
+            <!-- place navbar here -->
+        </header>
+        <main>
+
+            <div class="d-flex justify-conten-center" 
+            style="border: 2px solid black; border-radius: 30px; margin-top:250px; margin-left:450px; width: fit-content; padding: 20px; text-align: center; background-color: cornsilk;" 
+            >
+                <form action="">
+
+                <div class="mb-3">
+                    <label for="" class="form-label">Usuario</label>
+                    <input
+                        type="text"
+                        class="form-control"
+                        name=""
+                        id=""
+                        aria-describedby="helpId"
+                        placeholder=""
+                        style="width: 300px;"
+                    />
+                    
+                </div>
+
+                <div class="mb-3">
+                    <label for="" class="form-label">Clave</label>
+                    <input
+                        type="text"
+                        class="form-control"
+                        name=""
+                        id=""
+                        aria-describedby="helpId"
+                        placeholder=""
+                        style="width: 300px;"
+                    />
+                </div>
+
+                <button class="btn btn-danger">Entrar</button>
+                <button class="btn btn-danger">Borrar</button>
+
+                <br>
+
+                <a href="">Olvido su contraseña?</a>
+
+                </form>
+        </div>
+            
+            
+
+        </main>
+        <footer>
+            <!-- place footer here -->
+        </footer>
+        <!-- Bootstrap JavaScript Libraries -->
+        <script
+            src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
+            integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r"
+            crossorigin="anonymous"
+        ></script>
+
+        <script
+            src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js"
+            integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+"
+            crossorigin="anonymous"
+        ></script>
+    </body>
+</html>
